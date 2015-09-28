38818d1ca37d9b9383c4ebcb71c66342b6d84237
Frederick Jansen <frederick.jansen@gmail.com>
2015-09-27 23:02:43 -0400

    Added focusin handlers for translation

--- js/app/ZombieTranslatorView.js
+++ js/app/ZombieTranslatorView.js
@@ -14,10 +14,10 @@ define(['jquery', 'app/ZombieTranslator'], function ($, ZombieTranslator) {
 
   ZombieTranslatorView.prototype.listenKeyInput = function () {
     var that = this;
-    this.$english.on('keyup', function (e) {
+    this.$english.on('keyup focusin', function (e) {
       that.$zombie.val(that.zombieTranslator.englishToZombie($('#english').val()));
     });
-    this.$zombie.on('keyup', function (e) {
+    this.$zombie.on('keyup focusin', function (e) {
       that.$english.val(that.zombieTranslator.zombieToEnglish($('#zombie').val()));
     });
   };
