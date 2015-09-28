74f6be1fac2b628ce66b20f3c039b6a0806d555b
Frederick Jansen <frederick.jansen@gmail.com>
2015-09-27 21:25:52 -0400

    Moved to RequireJS and hooked up view logic

--- js/app/ZombieTranslatorView.js
+++ js/app/ZombieTranslatorView.js
@@ -1,3 +1,23 @@
-require(['jquery', 'app/ZombieTranslator'], function() {
+define(['jquery', 'app/ZombieTranslator'], function($, ZombieTranslator) {
 
+  function ZombieTranslatorView(config) {
+    // initialize
+    console.log('ZombieTranslatorView constructor');
+     this.config = config || {};
+     this.$english = this.config.english || $('#english');
+     this.$zombie = this.config.zombie || $('#zombie');
+
+     this.zombieTranslator = new ZombieTranslator();
+
+     this.listenKeyInput();
+  }
+
+  ZombieTranslatorView.prototype.listenKeyInput = function() {
+    var that = this;
+    this.$english.on('keyup', function(e) {
+      that.$zombie.val(that.zombieTranslator.englishToZombie($('#english').val()));
+    });
+  };
+
+  return ZombieTranslatorView;
 });
