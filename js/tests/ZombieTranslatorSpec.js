32b8cf3062619a479272d0f6abe06ad3d71a3b4e
Frederick Jansen <frederick.jansen@gmail.com>
2015-09-29 02:06:48 -0400

    Added tests for zombie to english and fixed some descriptions

--- js/tests/ZombieTranslatorSpec.js
+++ js/tests/ZombieTranslatorSpec.js
@@ -1,10 +1,14 @@
 define(['ZombieTranslator'], function (ZombieTranslator) {
-  var zombieTranslator = new ZombieTranslator();
-  describe('ZombieTranslator', function () {
-    describe('englishToZombie single characters', function () {
 
+  var zombieTranslator = new ZombieTranslator();
+
+  /*
+   English to Zombie
+   */
+  describe('ZombieTranslator englishToZombie', function () {
+    describe('englishToZombie single characters', function () {
       describe('e', function () {
-        it('e should be RR', function () {
+        it('e should be rr', function () {
           expect(zombieTranslator.englishToZombie('e')).toBe('rr');
         });
         it('E should be RR', function () {
@@ -76,11 +80,13 @@ define(['ZombieTranslator'], function (ZombieTranslator) {
       describe('capitalization', function () {
         it('the first letter of a sentence should be capitalized', function () {
           expect(zombieTranslator.englishToZombie('. a')).toBe('. Hra');
+          expect(zombieTranslator.englishToZombie('? a')).toBe('? Hra');
+          expect(zombieTranslator.englishToZombie('! a')).toBe('! Hra');
         });
       });
     });
 
-    describe('englishToZombie full words', function() {
+    describe('englishToZombie full words', function () {
       it('JaZahn should be JhraZhrahn', function () {
         expect(zombieTranslator.englishToZombie('JaZahn')).toBe('JhraZhrahn');
       });
@@ -98,7 +104,7 @@ define(['ZombieTranslator'], function (ZombieTranslator) {
       });
     });
 
-    describe('englishToZombie sentences', function() {
+    describe('englishToZombie sentences', function () {
       it('JaZahn should be JhraZhrahn', function () {
         expect(zombieTranslator.englishToZombie('JaZahn')).toBe('JhraZhrahn');
       });
@@ -116,7 +122,7 @@ define(['ZombieTranslator'], function (ZombieTranslator) {
       });
     });
 
-    describe('englishToZombie exceptions', function() {
+    describe('englishToZombie exceptions', function () {
       it('30 should be 30', function () {
         expect(zombieTranslator.englishToZombie('30')).toBe('30');
       });
@@ -132,5 +138,74 @@ define(['ZombieTranslator'], function (ZombieTranslator) {
     });
   });
 
+  /*
+   Zombie to English
+   */
+  describe('ZombieTranslator zombieToEnglish', function () {
+    describe('zombieToEnglish single characters', function () {
+      describe('e', function () {
+        it('rr should be e', function () {
+          expect(zombieTranslator.zombieToEnglish('rr')).toBe('e');
+        });
+      });
+
+      describe('i', function () {
+        it('rrRr should be i', function () {
+          expect(zombieTranslator.zombieToEnglish('rrRr')).toBe('i');
+        });
+      });
+
+      describe('o', function () {
+        it('rrrRr should be o', function () {
+          expect(zombieTranslator.zombieToEnglish('rrrRr')).toBe('o');
+        });
+      });
+
+      describe('u', function () {
+        it('rrrrRr should be u', function () {
+          expect(zombieTranslator.zombieToEnglish('rrrrRr')).toBe('u');
+        });
+      });
+
+      describe('r end', function () {
+        it('rh at end of word should be r', function () {
+          expect(zombieTranslator.zombieToEnglish('rh')).toBe('r');
+        });
+
+      });
+
+      describe('r', function () {
+        it('r should be RR in other cases', function () {
+          expect(zombieTranslator.zombieToEnglish('RRx')).toBe('rx');
+        });
+      });
+
+      describe('s', function () {
+        it('SSS at end of word should be s', function () {
+          expect(zombieTranslator.zombieToEnglish('SSS')).toBe('s');
+        });
+      });
+
+      describe('z', function () {
+        it('ZZZ at end of word should be z', function () {
+          expect(zombieTranslator.zombieToEnglish('ZZZ')).toBe('z');
+        });
+      });
+
+      describe('a', function () {
+        it('hra should be a', function () {
+          expect(zombieTranslator.zombieToEnglish('hra')).toBe('a');
+        });
+      });
+
+      describe('capitalization', function () {
+        it('the first letter of a sentence should be lowercase', function () {
+          expect(zombieTranslator.zombieToEnglish('. Hra')).toBe('. a');
+          expect(zombieTranslator.zombieToEnglish('? Hra')).toBe('? a');
+          expect(zombieTranslator.zombieToEnglish('! Hra')).toBe('! a');
+        });
+      });
+    });
+  });
 
 });
